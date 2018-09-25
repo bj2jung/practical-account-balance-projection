eb5a74d01b6b7cf3747b4186574a3764e1d3fbe4
Brendon Jung <bj2jung@gmail.com>
2018-09-25 04:33:59 -0400

    progress

--- src/index.js
+++ src/index.js
@@ -7,11 +7,14 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      items: []
+      incomeItems: [],
+      expenseItems: [],
+      balance: 0,
+      startDate: ""
     };
-    // add bind here if needed
   }
 
+  // HANDLE SUBMISSION OF ITEM
   handleSubmit = e => {
     e.preventDefault();
 
@@ -23,26 +26,42 @@ class App extends React.Component {
       incomeOrExpense: e.target[4].checked
     };
 
-    // TRUE MEANS IT'S INCOME!!!
-    let incomeItemList = [];
-    let expenseItemList = [];
-
     if (inputObject.incomeOrExpense) {
-      incomeItemList = this.state.items.push(inputObject);
+      this.state.incomeItems.push(inputObject);
     } else {
-      expenseItemList = this.state.items.push(inputObject);
+      this.state.expenseItems.push(inputObject);
     }
-    // const updatedItemList = this.state.items.push(inputObject);
-    this.setState((this.state.items: incomeItemList));
-    console.log(inputObject.incomeOrExpense);
+
+    this.setState(
+      ((this.state.incomeItems: incomeItems),
+      (this.state.expenseItems: expenseItems))
+    );
+    console.log(this.state.incomeItems);
+  };
+
+  // HANDLE SUBMISSION OF STARTINGBALANCE
+  handleSubmitStartingBalance = e => {
+    e.preventDefault();
+
+    const submittedBalance = {
+      startDate: e.target[0].value,
+      balance: e.target[1].value
+    };
+
+    this.setState(
+      (this.state.balance: balance),
+      (this.state.startDate: startDate)
+    );
   };
 
   render() {
     return (
       <div>
         <h1 className="App-header App-title">Practical Balance Sheet</h1>
-        <ItemTable items={this.state.items} />
+        <ItemTable title="Income" items={this.state.incomeItems} />
+        <ItemTable title="Expense" items={this.state.expenseItems} />
         <InputBox handleSubmit={this.handleSubmit} />
+        <StartingBalanceBox handleSubmit={this.handleSubmitStartingBalance} />
       </div>
     );
   }
@@ -61,22 +80,29 @@ class ItemTable extends React.Component {
     }
 
     return (
-      <table>
-        <thead>
-          <tr>
-            <th>Description</th>
-            <th>Amount</th>
-            <th>Frequency</th>
-            <th>Start/Occurance</th>
-          </tr>
-        </thead>
-        <tbody>{rows}</tbody>
-      </table>
+      <div>
+        <h2>{this.props.title}</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>Description</th>
+              <th>Amount</th>
+              <th>Frequency</th>
+              <th>Start/Occurance</th>
+            </tr>
+          </thead>
+          <tbody>{rows}</tbody>
+        </table>
+      </div>
     );
   }
 }
 
 class ItemRow extends React.Component {
+  propTypes: {
+    description: ReactProptypes.string.isRequired
+  };
+
   render() {
     const description = this.props.item.description;
     const amount = this.props.item.amount;
@@ -95,43 +121,60 @@ class ItemRow extends React.Component {
 
 //ITEM TABLE END
 
-// let ITEM = [
-//   {
-//     description: "salary",
-//     amount: 50,
-//     frequency: "bi-weekly",
-//     startOrOccuranceDate: 2018
-//   },
-//   {
-//     description: "side cash",
-//     amount: 60,
-//     frequency: "bi-weekly",
-//     startOrOccuranceDate: 2018
-//   }
-// ];
-
 //INPUT BOX START
 class InputBox extends React.Component {
   render() {
     return (
-      <form className="form" onSubmit={this.props.handleSubmit}>
-        <input type="text" placeholder="Add description" />
-        <input type="text" placeholder="Add amount" />
-        <select name="frequency">
-          <option value="One-time">One-time</option>
-          <option value="Weekly">Weekly</option>
-          <option value="Bi-weekly">Bi-weekly</option>
-          <option value="Monthly">Monthly</option>
-          <option value="Annually">Annually</option>
-        </select>
-        <input type="text" placeholder="Add start/occurance date" />
-        <input name="incomeOrExpense" type="checkbox" checked={null} />
-        <button type="submit">Add Item</button>
-      </form>
+      <div>
+        <form className="form" onSubmit={this.props.handleSubmit}>
+          <h2>Add Item</h2>
+          <input name="description" type="text" placeholder="Add description" />
+          <input name="amount" type="text" placeholder="Add amount" />
+          <select name="frequency">
+            <option value="One-time">One-time</option>
+            <option value="Weekly">Weekly</option>
+            <option value="Bi-weekly">Bi-weekly</option>
+            <option value="Monthly">Monthly</option>
+            <option value="Annually">Annually</option>
+          </select>
+          <input
+            name="startOrOccuranceDate"
+            type="text"
+            placeholder="Add start/occurance date"
+          />
+          <input name="incomeOrExpense" type="checkbox" checked={null} />
+          <button type="submit" disabled={null}>
+            Add Item
+          </button>
+        </form>
+      </div>
     );
   }
 }
 
 //INPUT BOX END
+
+// STARTING BALANCE BOX START
+class StartingBalanceBox extends React.Component {
+  render() {
+    return (
+      <div>
+        <form
+          className="form"
+          onSubmit={this.props.handleSubmitStartingBalance}
+        >
+          <h2>Starting Balance</h2>
+          <input name="startDate" type="text" placeholder="Start date" />
+          <input name="balance" type="text" placeholder="Starting balance" />
+          <button type="submit" disabled={null}>
+            Update
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+// STARTING BALANCE BOX END
 
 ReactDOM.render(<App />, document.getElementById("root"));
